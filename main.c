605948690fb63facfa2c1a9c4ebab6b7637cb5d9
jku20 <jku-ben20@stuy.edu>
2020-10-15 15:31:02 -0400

    added the second part which I had forgotten to do

--- main.c
+++ main.c
@@ -42,13 +42,32 @@ int main() {
     a[1] = 2;
     a[2] = 3;
     int b[l];
-    
+
+    //made functions
     print_arr("a",a,l);
     print_arr("b",b,l);
     printf("average of a: %lf\n",average(a,l));
     arrcpy(a,b,l);
     print_arr("copied a to b", b, l);
     printf("len(\"hello\"): %d\n",len("hello"));
+
+    //library functions
+    char s2[7];
+    char s1[6] = "steve";
+    printf("s1: %s\n",s1);
+    printf("s2: %s\n",s2);
+    strcpy(s2,s1);
+    printf("copy s1 to s2: %s\n",s2);
+    strcat(s2,"a");
+    printf("add a to s2: %s\n",s2);
+    if(strcmp(s2,s1) > 0) {
+        printf("s2 is greater than s1\n");
+    } else {
+        printf("s2 is less than or equal to s1\n");
+    }
+    printf("the first t in s2 is at position: %ld\n",strchr(s2,'t')-s2);
+    //I don't know why strchr is listed twice so I will use it twice I guess?
+    printf("the first a in s2 is at position: %ld\n",strchr(s2,'a')-s2);
 
     return 0;
 }
